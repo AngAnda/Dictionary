3c20cbfd6f7f412b87efdb1c34c87e270af94fd6
Andreea Angelescu <anda.angelescu@gmail.com>
2024-03-07 23:05:44 +0200

    Finalized implementing logic for Quiz

--- ViewModels/QuizPageViewModel.cs
+++ ViewModels/QuizPageViewModel.cs
@@ -2,7 +2,10 @@
 using Dictionary.Models;
 using Dictionary.Repository;
 using Dictionary.Services;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Dictionary.ViewModels
@@ -15,6 +18,20 @@ namespace Dictionary.ViewModels
 
         private readonly RandomWordGenerator _randomWordGenerator;
         private ObservableCollection<WordDto> _words;
+
+
+        private List<string> _userInputList;
+        private string _currentUserInput;
+
+        public string UserInput
+        {
+            get { return _userInputList[CurrentIndex]; }
+            set
+            {
+                _userInputList[CurrentIndex] = value;
+                NotifyPropertyChanged(nameof(UserInput));
+            }
+        }
 
         public ObservableCollection<WordDto> Words
         {
@@ -39,9 +56,7 @@ namespace Dictionary.ViewModels
             set
             {
                 _currentIndex = value;
-                NotifyPropertyChanged(nameof(CurrentIndex));
-                NotifyPropertyChanged(nameof(CurrentWord));
-                NotifyPropertyChanged(nameof(ShowDescription));
+                UpdateProperties();
             }
         }
 
@@ -52,18 +67,65 @@ namespace Dictionary.ViewModels
 
         public string RightButtonContent
         {
-            get => (CurrentIndex == numberOfWords) ? "Finish" : "Next";
+            get => (CurrentIndex == numberOfWords-1)? "Finish" : "Next";
+        }
+
+        private void InitializeQuiz()
+        {
+            Words = _randomWordGenerator.GenerateRandomWords();
+            _userInputList = Enumerable.Repeat(string.Empty, numberOfWords).ToList();
+            CurrentIndex = 0;
         }
 
         public QuizPageViewModel()
         {
             _randomWordGenerator = new RandomWordGenerator(new WordRepository(), numberOfWords);
-            Words = _randomWordGenerator.GenerateRandomWords();
-            CurrentIndex = 0;
+            InitializeQuiz();
 
-            NextWordCommand = new RelayCommand(_ => CurrentIndex++, _ => CurrentIndex < numberOfWords - 1);
+
+            NextWordCommand = new RelayCommand(ExecuteNextWordCommand, _ => true) ;
             PreviousWordCommand = new RelayCommand(_ => CurrentIndex--, _ => CurrentIndex > 0);
         }
+
+        private void ExecuteNextWordCommand(object parameter)
+        {
+            if (CurrentIndex < numberOfWords - 1)
+            {
+                CurrentIndex++;
+            }
+            else
+            {
+                ShowScore();
+                InitializeQuiz();
+            }
+
+        }
+
+        private void ShowScore()
+        {
+            int score = CalculateScore();
+            MessageBox.Show($"Your score is {score} out of {numberOfWords}");
+        }
+
+        private int CalculateScore()
+        {
+            int score = 0;
+            for (int i = 0; i < numberOfWords; i++)
+            {
+                if (Words[i].Name == _userInputList[i]) score++;
+            }
+            return score;
+        }
+
+        private void UpdateProperties()
+        {
+            NotifyPropertyChanged(nameof(CurrentWord));
+            NotifyPropertyChanged(nameof(ShowDescription));
+            NotifyPropertyChanged(nameof(UserInput));
+            NotifyPropertyChanged(nameof(RightButtonContent));
+        }
+
+        
 
         public ICommand NextWordCommand { get; set; }
 
